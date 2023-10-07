c7a85027693d293ab0d2795d4152176385c60699
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-10-07 15:54:58 +0200

    const declarations

---
+++ src/basics/src/index.ts
@@ -0,0 +1,52 @@
+/** Primitive Types */
+let isOK: boolean = false
+let num: number = 5.8
+let hello: string = 'world'
+let noValue: undefined = undefined
+let doesNotExist: null = null
+let wish: symbol = Symbol('star')
+let bigNum: bigint = 24n
+
+/** Instance Types */
+let regexp: RegExp = new RegExp('ab+c')
+let array: Array<number> = [1, 1, 2, 3, 5, 8]
+let set: Set<number> = new Set([1, 2, 3, 5, 8])
+/** A first in first out collection */
+class Queue<T>{
+    private data: Array<T> = []
+    push(item: T): void { this.data.push(item) }
+    pop(): T | undefined { return this.data.shift() }
+}
+let queue: Queue<number> = new Queue()
+
+/** Arrays and Tuples */
+let array2: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let array3: number[] = [1, 2, 3, 4, 5]
+array2 = [1]
+array3 = [1, 2, 3]
+// array3='hello' // Error
+
+// Tuple
+let tuple: [number, number] = [0, 0]
+tuple = [1, 1]
+// tuple = [5] // Error
+// tuple = [3, 5, 7] // Error
+// tuple = ['hello', 'world'] // Error
+
+/** Object Types andf Type Aliases */
+type Point = { x: number, y: number }
+let center: Point = {
+    x: 0,
+    y: 0,
+}
+let unit: Point = {
+    x: 1,
+    y: 1,
+}
+
+/** const Declarations */
+const point: Point = { x: 0, y: 0 }
+// point = { x: 1, y: 1}  // Error
+// All other behaviors are still the same as 'let'
+point.x = 1
+point.y = 1
--- src/primitive/src/index.ts
+++
@@ -1,7 +0,0 @@
-let isOK: boolean = false
-let num: number = 5.8
-let hello: string = 'world'
-let noValue: undefined = undefined
-let doesNotExist: null = null
-let wish: symbol = Symbol('star')
-let bigNum: bigint = 24n
