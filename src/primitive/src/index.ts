4ce4be46ae11d754c9f107d9a244ce2532ff972d
Sylvain Pelissier <sylvain@onenetbeyond.org>
2023-10-07 15:22:52 +0200

    primitive types

---
+++ src/primitive/src/index.ts
@@ -0,0 +1,7 @@
+let isOK: boolean = false
+let num: number = 5.8
+let hello: string = 'world'
+let noValue: undefined = undefined
+let doesNotExist: null = null
+let wish: symbol = Symbol('star')
+let bigNum: bigint = 24n
